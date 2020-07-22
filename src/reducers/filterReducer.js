00a1a4e702b8d028dce97bfca4217e4934869fc3
Pervez alam <pervezalam777@gmail.com>
2020-07-22 16:17:48 +0530

    filter reducer test canse and implementation

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,5 +1,16 @@
-import { FILTERS_RECEIVED, ADD_COLOR_TO_FILTER, REMOVE_COLOR_FROM_FILTER, ADD_BRAND_TO_FILTER, REMOVE_BRAND_FROM_FILTER } from "../actions/filterActions";
+import { 
+  FILTERS_RECEIVED, 
+  ADD_COLOR_TO_FILTER, 
+  REMOVE_COLOR_FROM_FILTER, 
+  ADD_BRAND_TO_FILTER, 
+  REMOVE_BRAND_FROM_FILTER, 
+  ADD_MIN_PRICE_TO_FILTER,
+  REMOVE_MIN_PRICE_FROM_FILTER,
+  ADD_MAX_PRICE_TO_FILTER,
+  REMOVE_MAX_PRICE_FROM_FILTER
+} from "../actions/filterActions";
 
+export 
 const initialState = {
   filter_list:null,
   applied_filters:{}
@@ -51,6 +62,71 @@ function removeFilterOrReturnSameState(state, key, value){
   return state;
 }
 
+function isValueExitsInRange(options) {
+  let {filters, key, position} = options;
+  if(options.value) {
+    return (filters && filters[key] && filters[key][position] === options.value);
+  }
+  return filters && filters[key] && filters[key][position] > -1
+}
+
+function addRangeToFilter({filters, key, value, position}) {
+  let arr = [];
+  arr[position] = value;
+
+  if(filters == null){
+    return {[key]:arr};
+  }
+  if(filters[key]){
+    arr = [...filters[key]];
+    arr[position] = value;
+  }
+  return {
+    ...filters,
+    [key]: arr
+  }
+}
+
+//TODO: Either break it more function or
+// check better solution for the same.
+function removeRangeToFilter({filters, key, position}) {
+  let arr = [...filters[key]]
+  if((arr.length === 1 && position === 0) 
+    || (arr.length === 2 && (!arr[0] || arr[0] === -1) && position == 1)
+  ){
+    let cloneFilter = {...filters};
+    delete cloneFilter[key];
+    return cloneFilter
+  }
+  arr[position] = -1;
+  return {
+    ...filters,
+    [key]:arr
+  }
+}
+
+function addPriceFilterOrReturnSameState({state, key, value, position}) {
+  let filters = state.applied_filters
+  if(isValueExitsInRange({filters, key, value, position})){
+    return state;
+  }
+  return {
+    ...state,
+    applied_filters: addRangeToFilter({filters, key, value, position})
+  }
+}
+
+function removePriceFilterOrReturnSameState({state, key, position}) {
+  let filters = state.applied_filters
+  if(isValueExitsInRange({filters, key, position})){
+    return {
+      ...state,
+      applied_filters: removeRangeToFilter({filters, key, position})
+    }
+  }
+  return state;
+}
+
 const filterReducer = (state = initialState, {type, payload}) => {
   switch (type) {
     case FILTERS_RECEIVED:
@@ -66,6 +142,32 @@ const filterReducer = (state = initialState, {type, payload}) => {
       return addFilterOrReturnSameState(state, 'brand', payload);
     case REMOVE_BRAND_FROM_FILTER:
       return removeFilterOrReturnSameState(state, 'brand', payload);
+    case ADD_MIN_PRICE_TO_FILTER:
+      return addPriceFilterOrReturnSameState({
+        state, 
+        key:'price', 
+        value:payload,
+        position:0
+      });
+    case REMOVE_MIN_PRICE_FROM_FILTER:
+      return removePriceFilterOrReturnSameState({
+        state,
+        key:'price',
+        position:0
+      })
+    case ADD_MAX_PRICE_TO_FILTER:
+      return addPriceFilterOrReturnSameState({
+        state,
+        key:'price',
+        value:payload,
+        position: 1
+      })
+    case REMOVE_MAX_PRICE_FROM_FILTER:
+      return removePriceFilterOrReturnSameState({
+        state,
+        key:'price',
+        position:1
+      })
     default:
       return state;
   }
